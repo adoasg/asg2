ad4db8f8a2ce44d2e4d7f96488cb89bc69d98f4d
adoasg <155883205+adoasg@users.noreply.github.com>
2024-01-18 04:12:08 +0800

    Update V1.1.2__updated_ASG2.sql

--- new_product/V1.1.2__updated_ASG2.sql
+++ new_product/V1.1.2__updated_ASG2.sql
@@ -1,5 +1,5 @@
 USE SCHEMA NWT;
 -- Insert unitCost from PRODUCT_FRESH into PRODUCT
-INSERT INTO PRODUCT (unitCost)
-SELECT unitCost
+INSERT INTO PRODUCT ("unitCost")
+SELECT "unitCost"
 FROM PRODUCT_FRESH;
