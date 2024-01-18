eda6aa30bb1c7736ea8039ed3d55386c5034c654
adoasg <155883205+adoasg@users.noreply.github.com>
2024-01-18 14:34:30 +0800

    Update V1.1.2__updated_objects.sql

--- migrations/V1.1.2__updated_objects.sql
+++ migrations/V1.1.2__updated_objects.sql
@@ -1,9 +1,3 @@
 -- Add a new column to the Product table to store UnitCost
 ALTER TABLE Product
 ADD COLUMN UnitCost DECIMAL(10, 2);
-
--- Update the UnitCost column in the Product table with values from Product_Fresh
-UPDATE Product AS p
-UPDATE Product_Fresh AS pf
-SET p.UnitCost = pf.UnitCost
-WHERE p.ProductID = pf.ProductID;
